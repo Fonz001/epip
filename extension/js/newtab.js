321e9d372ba95d468ff8c76571f270051fffe077
Martin Boer <martin@myparcel.com>
2016-07-30 21:40:57 +0200

    materialized and improved the newtab

--- extension/js/newtab.js
+++ extension/js/newtab.js
@@ -2,41 +2,41 @@ $(function(){
   get_data(function(data) {
     console.log(data);
 
-    if(data.shares.length < 1)
-    {
-      var avatar  = '<img src="http://www.iconsfind.com/wp-content/uploads/2015/08/20150831_55e46b18e2a80.png">';
-      var name    = '<p><strong>Nothing was shared yet</strong></p><p></p>';
-      var comment = '<p><em>If others share links with you they will show up here</em></p>';
-      $('#preview').hide();
-    }
-    else
-    {
+    var epip = {
+      name: 'Team Epiphany',
+      comment: 'Nothing is shared yet.<br>If others share something with you, it will show up here.',
+      avatar: '',
+      url: 'http://www.epip.nl',
+      screenshot: '../img/logo.png'
+    };
+
+    if (0 != data.shares.length) {
       var share   = data.shares[0];
       var creator = data.users[share.creatorId];
-      var name    = creator.name;
-      var email   = creator.email;
-      var avatar  = creator.avatar;
 
-      // TMP
-      var avatar  = 'http://www.iconsfind.com/wp-content/uploads/2015/08/20150831_55e46b18e2a80.png';
-      if(email) {
-        avatar = 'http://www.gravatar.com/avatar/' + md5(email.toLowerCase());
+      epip.name = creator.name;
+      epip.comment = share.comment;
+      epip.avatar = creator.avatar + '">';
+      epip.url = share.url;
+      epip.screenshot = 'http://api.screenshotlayer.com/api/capture?access_key=d6ae0602784f7d1f84ca3e3fe98bc301&url=' + encodeURIComponent(share.url) + '&viewport=1440x900&width=1280';
+
+      // inject gravatar image
+      if(creator.email) {
+        epip.avatar = '<img src="http://www.gravatar.com/avatar/' + md5(creator.email.toLowerCase()) + '">';
       }
-      var avatar  = '<img src="' + avatar + '">';
-      var name    = '<p><strong>Shared by</strong></p><p>' + name + '</p>';
-      var comment = '<p><em>"' + share.comment + '"</em></p>';
-      var screenshot = 'http://api.screenshotlayer.com/api/capture?access_key=d6ae0602784f7d1f84ca3e3fe98bc301&url=' + encodeURIComponent(share.url) + '&viewport=1440x900&width=1280';
 
+      // overwrite screenshotlayer api with youtube image url
       var youtube = parseYoutube(share.url);
       if(youtube) {
-        screenshot = 'https://img.youtube.com/vi/' + youtube + '/0.jpg';
+        epip.screenshot = 'https://img.youtube.com/vi/' + youtube + '/0.jpg';
       }
-
-      var screenshot = '<a href="' + share.url + '"><img src="' + screenshot + '"><div class="gradient"><strong>check it out!</strong></div></a>';
     }
 
-    $('#author').append(avatar).append(name);
-      $('#preview').append(screenshot);
-      $('#comment').append(comment).width(($('#comment em').outerWidth() + 30) + 'px');  // width fix
+    $('#myname').append(' ' + data.user.name);
+    $('#name').append(epip.name);
+    $('#comment').append(epip.comment);
+    $('#avatar').prepend(epip.avatar);
+    $('#preview a').prop('href', epip.url);
+    $('#preview img').prop('src', epip.screenshot);
   });
 });
