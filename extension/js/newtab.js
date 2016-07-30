95c500a06def3a3cf5bb05b33e12968599329110
Martin Boer <martin@myparcel.com>
2016-07-30 05:38:44 +0200

    tabs to spaces + added email form field + js gravatar if email exists (needs md5 function)

--- extension/js/newtab.js
+++ extension/js/newtab.js
@@ -1,36 +1,40 @@
 $(function(){
+  get_data(
+  function(data)
+  {
+    console.log(data);
 
-	get_data(
-	function( data )
-	{
-		console.log(data);
-		
-		if(data.shares.length<1)
-		{
-			var avatar		= '<img src="http://www.iconsfind.com/wp-content/uploads/2015/08/20150831_55e46b18e2a80.png">';
-			var name		= '<p><strong>Nothing was shared yet</strong></p><p></p>';
-			var comment		= '<p><em>If others share links with you they will show up here</em></p>';
-			$("#preview").hide();
-		}
-		else
-		{
-			var share 		= data.shares[0];
-			var creator 	= data.users[share.creatorId];
-			var name		= creator.name;
-			var avatar		= creator.avatar;
+    if(data.shares.length < 1)
+    {
+      var avatar  = '<img src="http://www.iconsfind.com/wp-content/uploads/2015/08/20150831_55e46b18e2a80.png">';
+      var name    = '<p><strong>Nothing was shared yet</strong></p><p></p>';
+      var comment = '<p><em>If others share links with you they will show up here</em></p>';
+      $('#preview').hide();
+    }
+    else
+    {
+      var share   = data.shares[0];
+      var creator = data.users[share.creatorId];
+      var name    = creator.name;
+      var email   = creator.email;
+      var avatar  = creator.avatar;
 
-			// TMP
-			var avatar		= "http://www.iconsfind.com/wp-content/uploads/2015/08/20150831_55e46b18e2a80.png";
+      // TMP
+      var avatar  = 'http://www.iconsfind.com/wp-content/uploads/2015/08/20150831_55e46b18e2a80.png';
+      if(email) {
+        avatar = 'http://www.gravatar.com/avatar/' + md5(email.toLowerCase());
+      }
+      var avatar  = '<img src="' + avatar + '">';
+      var name    = '<p><strong>Shared by</strong></p><p>' + name + '</p>';
+      var comment = '<p><em>"' + share.comment + '"</em></p>';
+      var screenshot = 'http://api.screenshotlayer.com/api/capture?access_key=d6ae0602784f7d1f84ca3e3fe98bc301&url=' + encodeURIComponent(share.url) + '&viewport=1440x900&width=1280';
+      var screenshot = '<a href="' + share.url + '"><img src="' + screenshot + '"><div class="gradient"><strong>check it out!</strong></div></a>';
+    }
 
-			var avatar		= '<img src="' + avatar + '">';
-			var name		= '<p><strong>Shared by</strong></p><p>' + name + '</p>';
-			var comment		= '<p><em>"' + share.comment + '"</em></p>';
-			var screenshot	= 'http://api.screenshotlayer.com/api/capture?access_key=d6ae0602784f7d1f84ca3e3fe98bc301&url=' + encodeURIComponent(share.url) + '&viewport=1440x900&width=1280';
-			var screenshot	= '<a href="' + share.url + '"><img src="' + screenshot + '"><div class="gradient"><strong>check it out!</strong></div></a>';
-		}
-
-		$('#author') .append( avatar ).append( name );
-	    $('#preview').append( screenshot );
-	    $('#comment').append( comment ).width(($('#comment em').outerWidth() + 30) + 'px');  // width fix
-	},{url:'http://api.jquery.com/jquery.each/'});
- });
+    $('#author').append(avatar).append(name);
+      $('#preview').append(screenshot);
+      $('#comment').append(comment).width(($('#comment em').outerWidth() + 30) + 'px');  // width fix
+  }, {
+  	url: 'http://api.jquery.com/jquery.each/'
+  });
+});
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,77 +1,80 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+  var tabInfo = null;
 
-	var tabInfo = null;
+  get_current_tab(function(tab) {
+    tabInfo = tab;
 
-	get_current_tab( function( tab ) 
-	{
-		tabInfo = tab;
+    get_data(function(data) {
+      // determine if profile data exists
+      if (!data.user.name) {
+        $('#edit').addClass('show');
+      } else {
+        $('#input_name').val(data.user.name);
+        $('#input_email').val(data.user.email);
+        $('#name').text(data.user.name);
+      }
 
-		get_data(
-			function( data )
-			{
-				if( !data.user.name ) $('#edit').addClass('show');
-				else {
-					$('[name=name]').val(data.user.name);
-					$('#name').text(data.user.name);
-				}				 
+      // generate users to share with
+      $.each(data.users, function(index, value) {
+        var isSend = data.shared.indexOf(value.userId) >- 1;
+        $('#users').append('<button class="' + (isSend?'sent':'') + '" data-id="' + value.userId + '">' + value.name + '</button>');
+      });
+      $('#users button').on('click', share_tab);
 
-				$.each(data.users, function( index, value ) {
-					var isSend = data.shared.indexOf(value.userId)>-1;
-	  				$("#users").append('<button class="'+(isSend?'sent':'')+'" data-id="'+value.userId+'">'+value.name+'</button>');
-				});
-				$("#users button").on( "click", share_tab);
+      // show edit page action
+      $('#profile').on('click', function() {
+        $('#edit').addClass('show');
+      });
 
-				$("#profile").on( "click", function() 
-				{
-					$('#edit').addClass('show');
-				} );
+      // save edit form action
+      $("#save-name").on('click', function() {
+        var name = $('#input_name').val();
+        var email = $('#input_email').val();
 
-				$("#save-name").on( "click", function() 
-				{
-					var name = $('[name=name]').val();
+        $('#name').text(name);
+        $('[data-id=' + data.user.userId + ']').text(name);
 
-					if(name) $('#edit').removeClass('show');
-					
-					$('#name').text(name);
-					$('[data-id='+data.user.userId+']').text(name);
+        set_data({
+          name: name,
+          email: email
+        },function(data){
+          if (data.name) {
+            $('#edit').removeClass('show');
+          }
+        });
+      });
+    }, {
+      url:tabInfo.url
+    });
+  });
 
-					set_data({name:name},function( data ){});
-				} );
-			},
-			{
-				url:tabInfo.url
-			});
-	});
+  // SHARE TAB
+  function share_tab(event) {
+    event.stopPropagation();
 
-	// SHARE TAB
-	function share_tab(event)
-	{	
-		event.stopPropagation();
+    var e = $(event.target);
+    var data = {
+      url:     tabInfo.url,
+      title:   tabInfo.title,
+      comment: $('#comment').val(),
+      user:    e.attr('data-id')
+    };
 
-		var e = $(event.target);
+    if(e.hasClass('sent')) {
+      data.del = true;
+    }
 
-		var data = {
-			url:		tabInfo.url,
-			title:		tabInfo.title,
-			comment:	$('#comment').val(),
- 			user:		e.attr('data-id')
-		}
-
-		if(e.hasClass('sent')) data.del = true;
-
-		// SHARE TAB INFO
-		set_data(data,
-		// ON DONE SHARING
-		function( data )
-		{
-			if(e.hasClass('sent'))  e.removeClass('sent');
-			else					e.addClass('sent');
-			//alert(JSON.stringify(data));
-		});
-	}
-
+    // SHARE TAB INFO
+    set_data(data, function(data) {
+      if(e.hasClass('sent')) {
+        e.removeClass('sent');
+      } else {
+        e.addClass('sent');
+      }
+      //alert(JSON.stringify(data));
+    });
+  }
 });
-
 
 // https://developer.chrome.com/extensions/tabs#method-query
 // tab.url is only available if the "activeTab" permission is declared.
